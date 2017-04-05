87ff116feb0785c6ad453cfe49f7399fa559fc4c
dannymt97 <dannymt97@gmail.com>
2017-04-05 10:19:11 -0400

    Fixes to CREATE TABLE statements

--- dbinit.py
+++ dbinit.py
@@ -6,26 +6,9 @@ c = conn.cursor()
 
 c.execute('PRAGMA foreign_keys = ON;')
 
-c.execute('''CREATE TABLE students(
-                email TEXT PRIMARY KEY, 
-                name TEXT
-            )''')
-c.execute('''CREATE TABLE events(
-                name TEXT, 
-                time TEXT, 
-                PRIMARY KEY(name, time)
-            )''')
-c.execute('''CREATE TABLE records(
-                event_name TEXT, 
-                event_time TEXT, 
-                student_email TEXT, 
-                checkin_time TEXT, 
-                is_member BOOLEAN, 
-                is_volunteer BOOLEAN, 
-                is_board BOOLEAN, 
-                FOREIGN KEY(event_name, event_time) REFERENCES events(name, time), 
-                FOREIGN KEY(student_email) REFERENCES students(email)
-            )''')
+c.execute('CREATE TABLE students(email TEXT PRIMARY KEY, name TEXT, is_member BOOLEAN, is_volunteer BOOLEAN, is_board BOOLEAN)')
+c.execute('CREATE TABLE events(name TEXT, time TEXT, PRIMARY KEY(name, time))')
+c.execute('''CREATE TABLE records(event_name TEXT, event_time TEXT, student_email TEXT, checkin_time TEXT, FOREIGN KEY(event_name, event_time) REFERENCES events(name, time), FOREIGN KEY(student_email) REFERENCES students(email))''')
 
 conn.commit()
 conn.close()
