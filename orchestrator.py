c2268ca434672e4000d09a159da2218149b891d9
dannymt97 <dannymt97@gmail.com>
2017-04-11 19:19:56 -0400

    removed underscores from tex variable names-- tex compilers interpret them as math

--- orchestrator.py
+++ orchestrator.py
@@ -27,53 +27,51 @@ def standard_report(names=[], dates=[], daterange=(), include_emails=False):
 
     arrival_times = analyses.get_arrival_deltas(events_list)
     plotter.arrival_chart(arrival_times, 'arrival_times.png')
-    tex_vars['arrival_chart'] = './.working/arrival_times.png'
+    tex_vars['arrivalchart'] = './.working/arrival_times.png'
 
     distinct_attend = analyses.count_attendees(events_list, distinct_only=True)
-    tex_vars['distinct_all'] = distinct_attend['All']
-    tex_vars['distinct_board'] = distinct_attend['Board']
-    tex_vars['distinct_volunteers'] = distinct_attend['Volunteers']
-    tex_vars['distinct_members'] = distinct_attend['Members']
-    tex_vars['distinct_nonmembers'] = distinct_attend['Nonmembers']
+    tex_vars['distinctall'] = distinct_attend['All']
+    tex_vars['distinctboard'] = distinct_attend['Board']
+    tex_vars['distinctvolunteers'] = distinct_attend['Volunteers']
+    tex_vars['distinctmembers'] = distinct_attend['Members']
+    tex_vars['distinctnonmembers'] = distinct_attend['Nonmembers']
 
     total_attend = analyses.count_attendees(events_list)
-    tex_vars['total_all'] = total_attend['All']
-    tex_vars['total_board'] = total_attend['Board']
-    tex_vars['total_volunteers'] = total_attend['Volunteers']
-    tex_vars['total_members'] = total_attend['Members']
-    tex_vars['total_nonmembers'] = total_attend['Nonmembers']
+    tex_vars['totalall'] = total_attend['All']
+    tex_vars['totalboard'] = total_attend['Board']
+    tex_vars['totalvolunteers'] = total_attend['Volunteers']
+    tex_vars['totalmembers'] = total_attend['Members']
+    tex_vars['totalnonmembers'] = total_attend['Nonmembers']
 
     average_attend = analyses.count_attendees(events_list, average_attendance=True)
-    tex_vars['average_all'] = average_attend['All']
-    tex_vars['average_board'] = average_attend['Board']
-    tex_vars['average_volunteers'] = average_attend['Volunteers']
-    tex_vars['average_members'] = average_attend['Members']
-    tex_vars['average_nonmembers'] = average_attend['Nonmembers']
+    tex_vars['averageall'] = average_attend['All']
+    tex_vars['averageboard'] = average_attend['Board']
+    tex_vars['averagevolunteers'] = average_attend['Volunteers']
+    tex_vars['averagemembers'] = average_attend['Members']
+    tex_vars['averagenonmembers'] = average_attend['Nonmembers']
 
     event_attend = analyses.count_attendees(events_list, average_events=True)
-    tex_vars['event_all'] = event_attend['All']
-    tex_vars['event_board'] = event_attend['Board']
-    tex_vars['event_volunteers'] = event_attend['Volunteers']
-    tex_vars['event_members'] = event_attend['Members']
-    tex_vars['event_nonmembers'] = event_attend['Nonmembers']
+    tex_vars['eventall'] = event_attend['All']
+    tex_vars['eventboard'] = event_attend['Board']
+    tex_vars['eventvolunteers'] = event_attend['Volunteers']
+    tex_vars['eventmembers'] = event_attend['Members']
+    tex_vars['eventnonmembers'] = event_attend['Nonmembers']
 
     if include_emails:
-        tex_vars['include_emails'] = ''
-
-        tex_vars['email_board'] = ''
-        tex_vars['email_volunteers'] = ''
-        tex_vars['email_members'] = ''
-        tex_vars['email_nonmembers'] = ''
+        tex_vars['emailboard'] = ''
+        tex_vars['emailvolunteers'] = ''
+        tex_vars['emailmembers'] = ''
+        tex_vars['emailnonmembers'] = ''
 
         email_list = analyses.list_emails(events_list)
         for email in email_list['Board']:
-            tex_vars['email_board'] += email + '\n'
+            tex_vars['emailboard'] += email + '\n'
         for email in email_list['Volunteers']:
-            tex_vars['email_volunteers'] += email + '\n'
+            tex_vars['emailvolunteers'] += email + '\n'
         for email in email_list['Members']:
-            tex_vars['email_members'] += email + '\n'
+            tex_vars['emailmembers'] += email + '\n'
         for email in email_list['Nonmembers']:
-            tex_vars['email_nonmembers'] += email + '\n'
+            tex_vars['emailnonmembers'] += email + '\n'
 
     events_attendance = {}
     for event in events_list:
@@ -81,7 +79,7 @@ def standard_report(names=[], dates=[], daterange=(), include_emails=False):
         event_str = event[0] + ' ' + event_time.date().isoformat()
         events_attendance[event_str] = analyses.count_attendees([event])
     plotter.bar_chart(events_attendance, 'attendance.png')
-    tex_vars['attendance_chart'] = './.working/attendance.png'
+    tex_vars['attendancechart'] = './.working/attendance.png'
 
     texvar.write_tex_vars(tex_vars)
     conn.close()
@@ -108,59 +106,59 @@ def comparison_report(events_data_a, events_data_b, include_emails=False):
 
     # Create dictionary entries for numbers of distinct attendees
     distinct_attend_a = analyses.count_attendees(events_list_a, distinct_only=True)
-    tex_vars['distinct_a_all'] = distinct_attend_a['All']
-    tex_vars['distinct_a_board'] = distinct_attend_a['Board']
-    tex_vars['distinct_a_volunteers'] = distinct_attend_a['Volunteers']
-    tex_vars['distinct_a_members'] = distinct_attend_a['Members']
-    tex_vars['distinct_a_nonmembers'] = distinct_attend_a['Nonmembers']
+    tex_vars['distinctall'] = distinct_attend_a['All']
+    tex_vars['distinctaboard'] = distinct_attend_a['Board']
+    tex_vars['distinctavolunteers'] = distinct_attend_a['Volunteers']
+    tex_vars['distinctamembers'] = distinct_attend_a['Members']
+    tex_vars['distinctanonmembers'] = distinct_attend_a['Nonmembers']
     distinct_attend_b = analyses.count_attendees(events_list_b, distinct_only=True)
-    tex_vars['distinct_b_all'] = distinct_attend_b['All']
-    tex_vars['distinct_b_board'] = distinct_attend_b['Board']
-    tex_vars['distinct_b_volunteers'] = distinct_attend_b['Volunteers']
-    tex_vars['distinct_b_members'] = distinct_attend_b['Members']
-    tex_vars['distinct_b_nonmembers'] = distinct_attend_b['Nonmembers']
+    tex_vars['distinctball'] = distinct_attend_b['All']
+    tex_vars['distinctbboard'] = distinct_attend_b['Board']
+    tex_vars['distinctbvolunteers'] = distinct_attend_b['Volunteers']
+    tex_vars['distinctbmembers'] = distinct_attend_b['Members']
+    tex_vars['distinctbnonmembers'] = distinct_attend_b['Nonmembers']
 
     # Create dictionary entries for total numbers of attendees
     total_attend_a = analyses.count_attendees(events_list_a)
-    tex_vars['total_a_all'] = total_attend_a['All']
-    tex_vars['total_a_board'] = total_attend_a['Board']
-    tex_vars['total_a_volunteers'] = total_attend_a['Volunteers']
-    tex_vars['total_a_members'] = total_attend_a['Members']
-    tex_vars['total_a_nonmembers'] = total_attend_a['Nonmembers']
+    tex_vars['totalaall'] = total_attend_a['All']
+    tex_vars['totalaboard'] = total_attend_a['Board']
+    tex_vars['totalavolunteers'] = total_attend_a['Volunteers']
+    tex_vars['totalamembers'] = total_attend_a['Members']
+    tex_vars['totalanonmembers'] = total_attend_a['Nonmembers']
     total_attend_b = analyses.count_attendees(events_list_b)
-    tex_vars['total_b_all'] = total_attend_b['All']
-    tex_vars['total_b_board'] = total_attend_b['Board']
-    tex_vars['total_b_volunteers'] = total_attend_b['Volunteers']
-    tex_vars['total_b_members'] = total_attend_b['Members']
-    tex_vars['total_b_nonmembers'] = total_attend_b['Nonmembers']
+    tex_vars['totalball'] = total_attend_b['All']
+    tex_vars['totalbboard'] = total_attend_b['Board']
+    tex_vars['totalbvolunteers'] = total_attend_b['Volunteers']
+    tex_vars['totalbmembers'] = total_attend_b['Members']
+    tex_vars['totalbnonmembers'] = total_attend_b['Nonmembers']
 
     # Create dictionary entries for average number of attendees per event
     average_attend_a = analyses.count_attendees(events_list_a, average_attendance=True)
-    tex_vars['average_a_all'] = average_attend_a['All']
-    tex_vars['average_a_board'] = average_attend_a['Board']
-    tex_vars['average_a_volunteers'] = average_attend_a['Volunteers']
-    tex_vars['average_a_members'] = average_attend_a['Members']
-    tex_vars['average_a_nonmembers'] = average_attend_a['Nonmembers']
+    tex_vars['averageaall'] = average_attend_a['All']
+    tex_vars['averageaboard'] = average_attend_a['Board']
+    tex_vars['averageavolunteers'] = average_attend_a['Volunteers']
+    tex_vars['averageamembers'] = average_attend_a['Members']
+    tex_vars['averageanonmembers'] = average_attend_a['Nonmembers']
     average_attend_b = analyses.count_attendees(events_list_b, average_attendance=True)
-    tex_vars['average_b_all'] = average_attend_b['All']
-    tex_vars['average_b_board'] = average_attend_b['Board']
-    tex_vars['average_b_volunteers'] = average_attend_b['Volunteers']
-    tex_vars['average_b_members'] = average_attend_b['Members']
-    tex_vars['average_b_nonmembers'] = average_attend_b['Nonmembers']
+    tex_vars['averageball'] = average_attend_b['All']
+    tex_vars['averagebboard'] = average_attend_b['Board']
+    tex_vars['averagebvolunteers'] = average_attend_b['Volunteers']
+    tex_vars['averagebmembers'] = average_attend_b['Members']
+    tex_vars['averagebnonmembers'] = average_attend_b['Nonmembers']
 
     # Create dictionary entries for the average number of events an attendee has attended
     event_attend_a = analyses.count_attendees(events_list_a, average_events=True)
-    tex_vars['event_a_all'] = event_attend_a['All']
-    tex_vars['event_a_board'] = event_attend_a['Board']
-    tex_vars['event_a_volunteers'] = event_attend_a['Volunteers']
-    tex_vars['event_a_members'] = event_attend_a['Members']
-    tex_vars['event_a_nonmembers'] = event_attend_a['Nonmembers']
+    tex_vars['eventaall'] = event_attend_a['All']
+    tex_vars['eventaboard'] = event_attend_a['Board']
+    tex_vars['eventavolunteers'] = event_attend_a['Volunteers']
+    tex_vars['eventamembers'] = event_attend_a['Members']
+    tex_vars['eventanonmembers'] = event_attend_a['Nonmembers']
     event_attend_b = analyses.count_attendees(events_list_b, average_events=True)
-    tex_vars['event_b_all'] = event_attend_b['All']
-    tex_vars['event_b_board'] = event_attend_b['Board']
-    tex_vars['event_b_volunteers'] = event_attend_b['Volunteers']
-    tex_vars['event_b_members'] = event_attend_b['Members']
-    tex_vars['event_b_nonmembers'] = event_attend_b['Nonmembers']
+    tex_vars['eventball'] = event_attend_b['All']
+    tex_vars['eventbboard'] = event_attend_b['Board']
+    tex_vars['eventbvolunteers'] = event_attend_b['Volunteers']
+    tex_vars['eventbmembers'] = event_attend_b['Members']
+    tex_vars['eventbnonmembers'] = event_attend_b['Nonmembers']
 
     # Compare the overlap / lack thereof of the attendee groups
     comparison = analyses.compare_attendees(events_list_a, events_list_b)
@@ -168,65 +166,63 @@ def comparison_report(events_data_a, events_data_b, include_emails=False):
     attends_only_a = comparison['a_not_b']
     attends_only_b = comparison['b_not_a']
 
-    tex_vars['all_both'] = len(attends_both['All'])
-    tex_vars['board_both'] = len(attends_both['Board'])
-    tex_vars['volunteers_both'] = len(attends_both['Volunteers'])
-    tex_vars['members_both'] = len(attends_both['Members'])
-    tex_vars['nonmembers_both'] = len(attends_both['Nonmembers'])
+    tex_vars['allboth'] = len(attends_both['All'])
+    tex_vars['boardboth'] = len(attends_both['Board'])
+    tex_vars['volunteersboth'] = len(attends_both['Volunteers'])
+    tex_vars['membersboth'] = len(attends_both['Members'])
+    tex_vars['nonmembersboth'] = len(attends_both['Nonmembers'])
 
-    tex_vars['all_only_a'] = len(attends_only_a['All'])
-    tex_vars['board_only_a'] = len(attends_only_a['Board'])
-    tex_vars['volunteers_only_a'] = len(attends_only_a['Volunteers'])
-    tex_vars['members_only_a'] = len(attends_only_a['Members'])
-    tex_vars['nonmembers_only_a'] = len(attends_only_a['Nonmembers'])
+    tex_vars['allonlya'] = len(attends_only_a['All'])
+    tex_vars['boardonlya'] = len(attends_only_a['Board'])
+    tex_vars['volunteersonlya'] = len(attends_only_a['Volunteers'])
+    tex_vars['membersonlya'] = len(attends_only_a['Members'])
+    tex_vars['nonmembersonlya'] = len(attends_only_a['Nonmembers'])
 
-    tex_vars['all_only_b'] = len(attends_only_b['All'])
-    tex_vars['board_only_b'] = len(attends_only_b['Board'])
-    tex_vars['volunteers_only_b'] = len(attends_only_b['Volunteers'])
-    tex_vars['members_only_b'] = len(attends_only_b['Members'])
-    tex_vars['nonmembers_only_b'] = len(attends_only_b['Nonmembers'])
+    tex_vars['allonlyb'] = len(attends_only_b['All'])
+    tex_vars['boardonlyb'] = len(attends_only_b['Board'])
+    tex_vars['volunteersonlyb'] = len(attends_only_b['Volunteers'])
+    tex_vars['membersonlyb'] = len(attends_only_b['Members'])
+    tex_vars['nonmembersonlyb'] = len(attends_only_b['Nonmembers'])
 
     # optionally, generate email lists
     if include_emails:
-        tex_vars['include_emails'] = ''
-
-        tex_vars['email_board_both'] = ''
-        tex_vars['email_volunteers_both'] = ''
-        tex_vars['email_members_both'] = ''
-        tex_vars['email_nonmembers_both'] = ''
-        tex_vars['email_board_only_a'] = ''
-        tex_vars['email_volunteers_only_a'] = ''
-        tex_vars['email_members_only_a'] = ''
-        tex_vars['email_nonmembers_only_a'] = ''
-        tex_vars['email_board_only_b'] = ''
-        tex_vars['email_volunteers_only_b'] = ''
-        tex_vars['email_members_only_b'] = ''
-        tex_vars['email_nonmembers_only_b'] = ''
+        tex_vars['emailboardboth'] = ''
+        tex_vars['emailvolunteersboth'] = ''
+        tex_vars['emailmembersboth'] = ''
+        tex_vars['emailnonmembersboth'] = ''
+        tex_vars['emailboardonlya'] = ''
+        tex_vars['emailvolunteersonlya'] = ''
+        tex_vars['emailmembersonlya'] = ''
+        tex_vars['emailnonmembersonlya'] = ''
+        tex_vars['emailboardonlyb'] = ''
+        tex_vars['emailvolunteersonly_b'] = ''
+        tex_vars['emailmembersonly_b'] = ''
+        tex_vars['emailnonmembersonly_b'] = ''
 
         for email in attends_both['Board']:
-            tex_vars['email_board_both'] += email + '\n'
+            tex_vars['emailboardboth'] += email + '\n'
         for email in attends_both['Volunteers']:
-            tex_vars['email_volunteers_both'] += email + '\n'
+            tex_vars['emailvolunteersboth'] += email + '\n'
         for email in attends_both['Members']:
-            tex_vars['email_members_both'] += email + '\n'
+            tex_vars['emailmembersboth'] += email + '\n'
         for email in attends_both['Nonmembers']:
-            tex_vars['email_nonmembers_both'] += email + '\n'
+            tex_vars['emailnonmembersboth'] += email + '\n'
         for email in attends_only_a['Board']:
-            tex_vars['email_board_only_a'] += email + '\n'
+            tex_vars['emailboardonlya'] += email + '\n'
         for email in attends_only_a['Volunteers']:
-            tex_vars['email_volunteers_only_a'] += email + '\n'
+            tex_vars['emailvolunteersonlya'] += email + '\n'
         for email in attends_only_a['Members']:
-            tex_vars['email_members_only_a'] += email + '\n'
+            tex_vars['emailmembersonlya'] += email + '\n'
         for email in attends_only_a['Nonmembers']:
-            tex_vars['email_nonmembers_only_a'] += email + '\n'
+            tex_vars['emailnonmembersonlya'] += email + '\n'
         for email in attends_only_b['Board']:
-            tex_vars['email_board_only_b'] += email + '\n'
+            tex_vars['emailboardonlyb'] += email + '\n'
         for email in attends_only_b['Volunteers']:
-            tex_vars['email_volunteers_only_b'] += email + '\n'
+            tex_vars['emailvolunteersonlyb'] += email + '\n'
         for email in attends_only_b['Members']:
-            tex_vars['email_members_only_b'] += email + '\n'
+            tex_vars['emailmembersonly_b'] += email + '\n'
         for email in attends_only_b['Nonmembers']:
-            tex_vars['email_nonmembers_only_b'] += email + '\n'
+            tex_vars['emailnonmembersonlyb'] += email + '\n'
 
     events_attendance_a = {}
     events_attendance_b = {}
@@ -240,8 +236,8 @@ def comparison_report(events_data_a, events_data_b, include_emails=False):
         events_attendance_b[event_str] = analyses.count_attendees([event])
     plotter.bar_chart(events_attendance_a, 'attendance_a.png')
     plotter.bar_chart(events_attendance_b, 'attendance_b.png')
-    tex_vars['attendance_chart'] = './.working/attendance_a.png'
-    tex_vars['attendance_chart'] = './.working/attendance_b.png'
+    tex_vars['attendancecharta'] = './.working/attendance_a.png'
+    tex_vars['attendancechartb'] = './.working/attendance_b.png'
 
     texvar.write_tex_vars(tex_vars)
     conn.close()
